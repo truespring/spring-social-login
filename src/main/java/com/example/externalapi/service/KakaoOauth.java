e914bf768af4d8c626bb03d3d17a2012293c766f
truespring <true_spring_@naver.com>
2021-08-06 23:14:06 +0900

    [feat] add kakao login api

--- src/main/java/com/example/externalapi/service/KakaoOauth.java
+++ src/main/java/com/example/externalapi/service/KakaoOauth.java
@@ -1,11 +1,20 @@
 package com.example.externalapi.service;
 
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Component
@@ -16,8 +25,8 @@ public class KakaoOauth implements SocialOauth {
     private String KAKAO_SNS_BASE_URL;
     @Value("${sns.kakao.client.id}")
     private String KAKAO_SNS_CLIENT_ID;
-//    @Value("${sns.kakao.client.secret}")
-//    private String KAKAO_SNS_CLIENT_SECRET;
+    @Value("${sns.kakao.client.secret}")
+    private String KAKAO_SNS_CLIENT_SECRET;
     @Value("${sns.kakao.callback.url}")
     private String KAKAO_SNS_CALLBACK_URL;
     @Value("${sns.kakao.token.url}")
@@ -41,6 +50,25 @@ public class KakaoOauth implements SocialOauth {
 
     @Override
     public String requestAccessToken(String code) {
-        return null;
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+
+        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.set("code", code);
+        params.set("client_id", KAKAO_SNS_CLIENT_ID);
+        params.set("client_secret", KAKAO_SNS_CLIENT_SECRET);
+        params.set("redirect_uri", KAKAO_SNS_CALLBACK_URL);
+        params.set("grant_type", "authorization_code");
+
+        HttpEntity<MultiValueMap<String, Object>> restRequest = new HttpEntity<>(params, headers);
+
+        ResponseEntity<JSONObject> responseEntity =
+                restTemplate.postForEntity(KAKAO_SNS_TOKEN_BASE_URL, restRequest, JSONObject.class);
+
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+
+            return Objects.requireNonNull(responseEntity.getBody()).toString();
+        }
+        return "카카오 로그인 요청 처리 실패";
     }
 }
