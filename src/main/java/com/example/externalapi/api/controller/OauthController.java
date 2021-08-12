752498fdecac35c7dfdabbe3a1540d9c25bf67f4
truespring <true_spring_@naver.com>
2021-08-12 23:10:55 +0900

    [feat] adding logout function

--- src/main/java/com/example/externalapi/api/controller/OauthController.java
+++ src/main/java/com/example/externalapi/api/controller/OauthController.java
@@ -4,16 +4,21 @@ import com.example.externalapi.api.constants.SocialLoginType;
 import com.example.externalapi.api.service.OauthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
-@RestController
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
 @CrossOrigin
 @RequiredArgsConstructor
 @RequestMapping(value = "/auth")
 @Slf4j
 public class OauthController {
     private final OauthService oauthService;
+    private final Map<String, Object> returnMap = new HashMap<>();
 
     /**
      * 사용자로부터 SNS 로그인 요청을 Social Login Type 을 받아 처리
@@ -43,6 +48,13 @@ public class OauthController {
             @PathVariable(name = "socialLoginType") SocialLoginType socialLoginType,
             @RequestParam(name = "code") String code) {
         log.info(">> 소셜 로그인 API 서버로부터 받은 code :: {}", code);
-        return oauthService.requestAccessToken(socialLoginType, code);
+        returnMap.put("accessToken", oauthService.requestAccessToken(socialLoginType, code));
+        returnMap.put("socialLoginType", socialLoginType);
+        return "success";
+    }
+
+    @GetMapping(value = "/success")
+    public @ResponseBody Map<String, Object> callbackPage() {
+        return returnMap;
     }
 }
