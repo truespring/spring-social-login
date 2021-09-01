99b02906ce783be60f86b3012d0c17d561125f21
truespring <truespring1@gmail.com>
2021-09-01 21:57:47 +0900

    [feat] refactoring code

--- src/main/java/com/example/externalapi/api/service/GithubOauth.java
+++ src/main/java/com/example/externalapi/api/service/GithubOauth.java
@@ -4,7 +4,6 @@ import com.example.externalapi.api.domain.github.GithubOauthResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
